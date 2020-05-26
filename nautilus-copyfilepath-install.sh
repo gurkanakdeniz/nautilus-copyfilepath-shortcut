b1868e9226b4adfc9950cf0f9fa7ffd276edf43a
gurkanakdeniz <akdenizgurkan@gmail.com>
2020-05-26 17:33:29 +0300

    review

--- nautilus-copyfilepath-install.sh
+++ nautilus-copyfilepath-install.sh
@@ -5,12 +5,15 @@ filename="scripts-accels"
 shortcut="<Control><Shift>c copypath"
 FILE=~/.config/nautilus/$filename
 if [ -f "$FILE" ]; then
-    echo "\n$shortcut" >> "$nautilusconfigpath$filename"
+   if grep -q "$shortcut" "$nautilusconfigpath$filename" ; then 
+      echo "copypath shortcut found ignore shortcut write.."
+   else
+      echo "\n$shortcut" >> "$nautilusconfigpath$filename"
+   fi
 else 
    touch $nautilusconfigpath$filename
-   echo "$shortcut" >> "$nautilusconfigpath$filename"
+   echo -n "$shortcut" >> "$nautilusconfigpath$filename"
 fi
-
 
 nautilusscriptpath=~/.local/share/nautilus/scripts/
 filename="copypath"
@@ -19,9 +22,11 @@ rm -rf $nautilusscriptpath$filename
 touch $nautilusscriptpath$filename
 chmod +x $nautilusscriptpath$filename
 
-echo '# !/bin/sh
+echo -n '# !/bin/sh
 
 filepath="$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"
 echo -n "$filepath" | tr -d '"'"'\\n'"'"' | xsel -b -i' >> $nautilusscriptpath$filename
 
 nautilus -q
+
+echo "success return of the jedi o-o-"
